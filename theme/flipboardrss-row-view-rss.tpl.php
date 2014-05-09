bc456c67ad04ee2e512b4412d539d0843762089f
tomogden <doswebdev@gmail.com>
2014-05-09 12:25:43 -0400

    Initial commit of code.

---
+++ theme/flipboardrss-row-view-rss.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Default view template to display a item in an RSS feed.
+ *
+ * @ingroup views_templates
+ */
+drupal_set_message("fb row template");
+?>
+  <item>
+    <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
+    <description><?php //print $description; ?></description>
+    <?php print $item_elements; ?>
+  </item>
