c2496bbf992dd19a842e925b10dcae659be37c3e
Thomas J Ogden <tomogden@Thomass-MacBook-Pro.local>
2014-06-11 16:29:03 -0400

    Correct name spaces per advice from Superfeedr.

--- theme/flipboardrss-view-rss.tpl.php
+++ theme/flipboardrss-view-rss.tpl.php
@@ -7,18 +7,19 @@
  * @ingroup views_templates
  */
 ?>
-<?php print "<?xml"; ?> version="2.0" encoding="utf-8" <?php print "?>\n"; ?>
+<?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>\n"; ?>
 <rss version="2.0"
   xml:base="<?php print $link; ?>"
   <?php print $namespaces; ?>>
   <channel>
     <title><?php print $title; ?></title>
-    <link><?php print $link; ?></link>
-    <link rel="hub" href="<?php print $hub_url; ?>" xmlns="http://www.w3.org/200/Atom" />
-    <link rel="self" href="<?php print $feed_url; ?>" xmlns="http://www.w3.org/200/Atom" />
     <description><?php print $description; ?></description>
     <language><?php print $langcode; ?></language>
     <?php print $channel_elements; ?>
+    <!-- PubSubHubbub Discovery -->
+    <link rel="hub" href="<?php print $hub_url; ?>" />
+    <link rel="self" href="<?php print $feed_url; ?>" />
+    <!-- End Of PubSubHubbub Discovery -->
     <?php print $items; ?>
   </channel>
 </rss>
