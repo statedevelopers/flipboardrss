59e6544234b9f4416e16cfca56b96a8aada346f3
Thomas J Ogden <tomogden@Thomass-MacBook-Pro.local>
2014-06-13 15:34:16 -0400

    Make corrections to name spaces.

--- theme/flipboardrss-view-rss.tpl.php
+++ theme/flipboardrss-view-rss.tpl.php
@@ -9,16 +9,16 @@
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>\n"; ?>
 <rss version="2.0"
-  xml:base="<?php print $link; ?>"
   <?php print $namespaces; ?>>
   <channel>
     <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
     <description><?php print $description; ?></description>
     <language><?php print $langcode; ?></language>
     <?php print $channel_elements; ?>
     <!-- PubSubHubbub Discovery -->
-    <link rel="hub" href="<?php print $hub_url; ?>" />
-    <link rel="self" href="<?php print $feed_url; ?>" />
+    <link rel="hub" href="<?php print $hub_url; ?>" xmlns="http://www.w3.org/2005/Atom" />
+    <link rel="self" href="<?php print $feed_url; ?>" xmlns="http://www.w3.org/2005/Atom" />
     <!-- End Of PubSubHubbub Discovery -->
     <?php print $items; ?>
   </channel>
